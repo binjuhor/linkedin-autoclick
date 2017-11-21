d40d96db08990a5516c7c01d7b9d9c658897ff05
Binjuhor <binjuhor@gmail.com>
2017-11-21 08:29:18 +0700

    Remove the clicked for new items to connect

--- clickLinkIn.js
+++ clickLinkIn.js
@@ -1,13 +1,17 @@
 setInterval(function () {
     jQuery('.mn-person-info').each(function () {
-        var button = jQuery(this).find('.button-secondary-small');
-        var friendName = jQuery(this).find('.mn-person-info__name').text();
+        var button      = jQuery(this).find('.button-secondary-small');
+        var friendName  = jQuery(this).find('.mn-person-info__name').text();
+        var toScroll    = jQuery(document).height();
         if (jQuery(button).length) {
-            jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, 500);
             if (!jQuery(button).hasClass('clicked')) {
                 jQuery(button).click();
                 console.log('Sent connect with ' + friendName.trim());
                 jQuery(button).addClass('clicked');
+            }
+            if (jQuery(button).hasClass('clicked')) {
+                jQuery("html, body").animate({ scrollTop: toScroll }, 0);
+                jQuery(button).closest('li').remove();
             }
         }
     })
