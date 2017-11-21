24cd706078ca6d81bfd2db01cc4f14bd144d75dd
Binjuhor <binjuhor@gmail.com>
2017-11-21 07:48:35 +0700

    Reduce the error when re-clicked on the connect button

--- clickLinkIn.js
+++ clickLinkIn.js
@@ -1,11 +1,14 @@
 setInterval(function () {
-    jQuery('.mn-person-info').each(function(){
+    jQuery('.mn-person-info').each(function () {
         var button = jQuery(this).find('.button-secondary-small');
         var friendName = jQuery(this).find('.mn-person-info__name').text();
-        if ($(button).length ){
+        if (jQuery(button).length) {
             jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, 500);
-            jQuery(button).click();
-            console.log('Sent connect with ' + friendName.trim());
+            if (!jQuery(button).hasClass('clicked')) {
+                jQuery(button).click();
+                console.log('Sent connect with ' + friendName.trim());
+                jQuery(button).addClass('clicked');
+            }
         }
     })
 }, 5000);
